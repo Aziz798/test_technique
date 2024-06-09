8f33cfe71e655e59c3b33e8c29cfb6acad84861a
Aziz798 <bouazizimohamedaziz76@gmail.com>
2024-06-09 01:38:58 +0100

    initial commit

--- backend/backend/settings.py
+++ backend/backend/settings.py
@@ -91,8 +91,12 @@ MEDIA_ROOT = BASE_DIR / 'media'
 
 DATABASES = {
     'default': {
-        'ENGINE': 'django.db.backends.sqlite3',
-        'NAME': BASE_DIR / 'db.sqlite3',
+        'ENGINE': 'django.db.backends.postgresql',
+        'NAME': 'test_technique',
+        'USER': 'postgres',
+        'PASSWORD': 'root',
+        'HOST': 'localhost',
+        'PORT': '5432',
     }
 }
 
